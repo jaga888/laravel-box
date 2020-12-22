0ab2be79e3ce6e6323b7a764fb6c74ee022f91b0
golubka <jokkerit3@gmail.com>
2020-12-22 12:21:57 +0300

    slug | created custom generator slug --routing

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\DB;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +17,43 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])
+    ->name('home');
+
+$uri = '';
+
+foreach (Request::segments() as $segment) {
+    $uri .= '/' . $segment;
+}
+
+$page = null;
+
+if (Schema::hasTable('slug__slugs')) {
+    $page = DB::table('pages')
+        ->where('slug', '=', $uri)
+        ->first();
+}
+
+if ($page) {
+    $pageType= DB::table('page_types')
+        ->where('id', '=', $page->page_type_id)
+        ->first();
+
+    if ($page->is_main) {
+        Route::get($page->slug, [$pageType->controller, $pageType->action]);
+        request()->merge(['entityId' => $page->entity_id]);
+    } else {
+        $pageMain = DB::table('pages')
+            ->where('entity_id', '=', $page->entity_id)
+            ->where('page_type_id', '=', $page->page_type_id)
+            ->where('is_main', '=', 1)
+            ->first();
+
+        if ($pageMain) {
+            Route::redirect($page->slug, $pageMain->slug);
+        }
+    }
+}
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
